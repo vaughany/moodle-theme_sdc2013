7bc1a34070c5a10a6177e894cb2b975dbb4bf4d6
Paul Vaughan <paulvaughan@southdevon.ac.uk>
2013-07-18 10:21:07 +0100

    Addition of general.php layout to make the profile pic (and Google Analytics) appear throughout the site.

--- version.php
+++ version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2013070900;
+$plugin->version   = 2013071800;
 $plugin->requires  = 2013050100;
 $plugin->component = 'theme_sdc2013';
 $plugin->dependencies = array(
